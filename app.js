9155c12112fae9cf9397541b49ab62cd0fdcaebc
Alice Post <alicep2322@gmail.com>
2020-01-25 22:21:47 -0700

    Add endpoint for fetching footnotes for specific paper

--- app.js
+++ app.js
@@ -41,6 +41,23 @@ app.get('/api/v1/papers/:id', (request, response) => {
     });
 });
 
+// GET a specific footnote
+app.get('/api/v1/papers/:id/footnotes', (request, response) => {
+  database('footnotes').where('paper_id', request.params.id).select()
+    .then(footnotes => {
+      if (footnotes.length) {
+        response.status(200).json(footnotes);
+      } else {
+        response.status(404).json({
+          error: `Could not find footnotes for paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
 app.get('/api/v1/footnotes', (request, response) => {
   database('footnotes').select().then((footnotes) => {
     response.status(200).json(footnotes);
